7068eadd22279dc6165730384b5461de66ab7e55
Zachary Lehmann <zlehmann@gmail.com>
2019-09-02 13:05:30 -0400

    adding background images.

--- src/App.js
+++ src/App.js
@@ -4,9 +4,19 @@ import NavContainer from './components/NavContainer.js'
 
 
 function App() {
+  let bgImage = "/env_fenceline.JPG"
+  let style = {
+    width: "100%",
+    height: "400px",
+    marginRight: "auto",
+    marginLeft: "auto",
+    maxWidth: "980px",
+    backgroundImage: 'url(' + (process.env.PUBLIC_URL + bgImage) + ')',
+    backgroundSize: "cover"
+  }
 
   return (
-    <div id="App">
+    <div id="App" style={style}>
       <NavContainer />
     </div>
   );
