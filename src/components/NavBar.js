d0d7c30ccdec340721e2eb0b930e2f67ac2e49fe
Zachary Lehmann <zlehmann@gmail.com>
2019-09-02 12:04:01 -0400

    fixed navbar sizing dynamically

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,9 +3,18 @@ import React, {Component} from 'react'
 class NavBar extends Component {
 
   render() {
+    const style = {
+      width: "100%",
+      backgroundColor: "rgba(0,0,0, 0.4)",
+      height: (150 * this.props.scale),
+      marginLeft: (300 * this.props.scale),
+      marginTop: (75 * this.props.scale)
+    }
+
+
     if(this.props.currentSelection === "tech") {
       return (
-        <div id="navBar">
+        <div id="navBar" style={style}>
           <ul className="techNavLinks">
             <li>tech placeholder</li>
           </ul>
@@ -13,7 +22,7 @@ class NavBar extends Component {
       )
     } else if(this.props.currentSelection === "env") {
       return (
-        <div id="navBar">
+        <div id="navBar" style={style}>
           <ul className="envNavLinks">
             <li>env placeholder</li>
           </ul>
@@ -21,7 +30,7 @@ class NavBar extends Component {
       )
     } else if(this.props.currentSelection === "int") {
       return (
-        <div id="navBar">
+        <div id="navBar" style={style}>
           <ul className="intNavLinks">
             <li>int placeholder</li>
           </ul>
