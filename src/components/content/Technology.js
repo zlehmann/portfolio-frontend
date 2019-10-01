66aaed5413959e4939e40d464ab7487766b798df
Zachary Lehmann <zlehmann@gmail.com>
2019-09-30 20:56:57 -0400

    updating text on technology page.

--- src/components/content/Technology.js
+++ src/components/content/Technology.js
@@ -5,13 +5,11 @@ class Technology extends Component {
     return(
       <div className="contentHeader">
         <h1>Zachary Lehmann</h1>
-        <p>My dual passions for the environment and technology have taken
-        me on some interesting journeys. I have spent the past 15 years working
-        with wetlands and forested habitats all over the east coast. I have developed
-        Unmanned Aerial System platforms for wetland re-vegetation surveys and
-        created sea-level rise models using GIS software. Now after graduating from
-        the Flatiron web-developer boot camp I'm looking to transition my career
-        full time. </p>
+        <p>In the summer of 2018 I decided that I wanted to transition careers. My experiences in the
+        environmental sector had left me disenchanted with the industry and a lack of creativity in my
+         day to day job had me feeling unfulfilled. I enrolled in the Flatiron program that September, and
+         nine months later, graduated from the self-paced web development program. Now I spend my time
+         building web apps and looking for the opportunity to transition to a full time job as a developer. </p>
       </div>
     )
   }
