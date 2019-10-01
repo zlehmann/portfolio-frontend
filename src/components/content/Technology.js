04d22c493ee1f9b986fb98c2cb54143b6bf90e39
Zachary Lehmann <zlehmann@gmail.com>
2019-09-30 21:08:11 -0400

    adds title on tech page.

--- src/components/content/Technology.js
+++ src/components/content/Technology.js
@@ -4,7 +4,7 @@ class Technology extends Component {
   render () {
     return(
       <div className="contentHeader">
-        <h1>Zachary Lehmann</h1>
+        <h1>Full-Stack Web Developer</h1>
         <p>In the summer of 2018 I decided that I wanted to transition careers. My experiences in the
         environmental sector had left me disenchanted with the industry and a lack of creativity in my
          day to day job had me feeling unfulfilled. I enrolled in the Flatiron program that September, and
